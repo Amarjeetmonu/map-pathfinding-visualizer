276126a3ae31899c92b7ef9d427f653487e9ca0c
Amarjeetmonu <162833903+Amarjeetmonu@users.noreply.github.com>
2026-08-24 01:40:56 +0530

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -19,18 +19,17 @@ from algorithms import (
 
 app = FastAPI()
 
-
-# ==========================================
-# CORS
-# ==========================================
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://map-pathfinding-visualizer1.onrender.com",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # ==========================================
 # HOME
